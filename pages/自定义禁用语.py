16fb564b1e1a43c58313814ced45402417c2e9eb
martin.l <martinbuz@163.com>
2023-12-26 18:05:43 +0800

    fix new page

--- pages/自定义禁用语.py
+++ pages/自定义禁用语.py
@@ -1,5 +1,30 @@
-import streamlit as st'
+import streamlit as st
 import pandas as pd
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if st.session_state.get("password_correct", False):
+        return True
+
+    st.text_input(
+        "请输入密码", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("密码错误")
+    return False
+
+
+if not check_password():
+    st.stop()
+
 data = None
 with open('pages/forbidden.txt','r',encoding='utf-8-sig') as fp:
     data = fp.readlines()
